1dfb8b34f4f823d5bb5171d1a398e787a34344a9
Sin-Dong-Won <56111933+Sin-Dong-Won@users.noreply.github.com>
2021-09-16 17:30:42 +0900

    I make Drill03

--- character_rectangular_and_circle.py
+++ character_rectangular_and_circle.py
@@ -17,14 +17,14 @@ def rectangular_move():
 
     while(x < 800):
         clear_canvas_now()
-        #grass.draw_now(400, 30)
+        grass.draw_now(400, 30)
         character.draw_now(x, y)
         x = x + 5
         delay(0.01)
 
     while(y < 600):
         clear_canvas_now()
-       # grass.draw_now(400, 30)
+        grass.draw_now(400, 30)
         character.draw_now(x, y)
         y = y + 5
         delay(0.01)
@@ -32,14 +32,14 @@ def rectangular_move():
 
     while(x > 0):
         clear_canvas_now()
-        #grass.draw_now(400, 30)
+        grass.draw_now(400, 30)
         character.draw_now(x, y)
         x = x - 5
         delay(0.01)
 
     while(y > 90):
         clear_canvas_now()
-        #grass.draw_now(400, 30)
+        grass.draw_now(400, 30)
         character.draw_now(x, y)
         y = y - 5
         delay(0.01)  
@@ -48,12 +48,14 @@ def circle_move():
 
     for i in range(0, 360):
         clear_canvas_now()
-        #grass.draw_now(400, 30)
+        grass.draw_now(400, 30)
         character.draw_now(math.sin(Pi * (180 - i)) * Wid / 2 + Wid / 2, math.cos(Pi * (180 - i)) * Hei / 2 + Hei / 2 + 90)
         delay(0.01) 
 
 
 while(True):
+    clear_canvas_now()
+    rectangular_move()
     circle_move()
         
 
